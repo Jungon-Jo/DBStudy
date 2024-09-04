e84d6bc952a200386bbb3c4d4f0f35e757b1843b
Jungon Jo <jojungon@gmail.com>
2024-09-04 12:19:59 +0900

    문제 풀이

--- test/데이터 추출/24.09.03_DB Group By_test/DB_Test00.sql
+++ test/데이터 추출/24.09.03_DB Group By_test/DB_Test00.sql
@@ -9,8 +9,7 @@ time int);
 
 create sequence muser_no
 increment by 1
-start with 10
-;
+start with 10;
 
 // 데이터 입력
 insert into muser values(muser_no.nextval,'870205-1','이승진',1,10000,34);
@@ -31,13 +30,24 @@ insert into muser values(muser_no.nextval,'670805-1','현진수',2,20000,34);
 insert into muser values(muser_no.nextval,'840207-1','최이런',1,10000,35);
 insert into muser values(muser_no.nextval,'770405-1','이천안',1,10000,31);
 
+select * from muser;
+
 // 문제
 // 1. grade가 3인 사람은 몇명인가요?
 select count(*) 인원수 from muser where grade=3;
+select count(*)
+from muser
+where grade=3;
 -- select절에 grade컬럼을 표출시키기 위해서는 group by를 사용하여 grade 그룹을 형성해주어야 한다.
 
 // 2. grade가 1, 2, 4인 사람의 salary의 평균을 구하시오.
 select avg(salary) 평균봉급 from muser where grade=1 or grade=2 or grade=4;
+select avg(salary)
+from muser
+where grade in (1,2,4);
+//where grade=1 or grade=2 or grade=4;
+// group by  -- 정의 : 그룹별 집계일 경우
+// order by
 
 // 3. salary가 20000 미만인 사람은 총 몇명입니까?
 select count(*) 인원수 from muser where salary<20000;
@@ -48,21 +58,75 @@ select avg(salary) 평균봉급 from muser where salary>=30000;
 // 5. 77년생 중에 salary가 가장 적은 사람의 이름과 나이와 salary를 출력하시오.
 select name, 124-substr(reg_num,1,2) age, salary 
     from muser 
-    where substr(reg_num,1,2)=77 and salary=(select min(salary) from muser where substr(reg_num,1,2)=77);
+    where substr(reg_num,1,2)='77' and salary=(select min(salary) from muser where substr(reg_num,1,2)='77');
+    
+select min(salary)
+from muser
+where substr(reg_num,1,2)='77';
 
+-- 77년생의 데이터를 뽑아본다.
+select substr(reg_num,1,2) from muser;
+
+-- 77년생 중 가장 작은 salary를 뽑아 내본다.
+select min(salary)
+from muser
+where substr(reg_num,1,2)='77';
+
+-- 정답(하지만 where절의 salary의 값은 계속 변할 수 있는 값이므로, 상수값 대신 다른 값으로 써주는것이 좋다.)
+select name, 124-substr(reg_num,1,2) age, salary 
+    from muser 
+    where substr(reg_num,1,2)='77' and salary=10000;
+    
+-- 수식, 함수 모두 사용 불가하므로, 서브쿼리를 사용해준다.(77년생 중 가장 작은 salary를 뽑아낸 쿼리를 서브쿼리로 사용)
+select name, 124-substr(reg_num,1,2) age, salary 
+    from muser 
+    where substr(reg_num,1,2)='77' and salary=
+                                            (select min(salary)
+                                            from muser
+                                            where substr(reg_num,1,2)='77');
+                                            
+-- 나이를 출력하려고 하는데 주민번호밖에 없고, 분석함수를 사용하기 위해서는 네자리의 출생년도가 필요하다.
+select 1900+substr(reg_num,1,2) from muser;
+-- 2000년대 출생자들의 경우 오류가 발생
+-- 2000년대 출생자들의 주민번호 뒷자리의 첫번째 수가 3과 4로 시작하는것을 이용하여 구분해서 값을 계산해준다.
+select substr(reg_num,1,2) a, 
+       decode(substr(reg_num,8,1),1,1900,2,1900,2000) b 
+       from muser;
+select substr(reg_num,1,2) a, 
+       decode(substr(reg_num,8,1),1,1900,2,1900,2000) b,
+       decode(substr(reg_num,8,1),1,1900,2,1900,2000)+substr(reg_num,1,2) c 
+       from muser;
+       
+-- 최종 답안
+select name 이름,
+       extract (year from sysdate)-(decode(substr(reg_num,8,1),1,1900,2,1900,2000)+substr(reg_num,1,2)) 나이,
+       salary 급여
+       from muser 
+       where substr(reg_num,1,2)='77'
+             and salary=(select min(salary)
+                                from muser
+                                where substr(reg_num,1,2)='77');
+    
 // 6. 모든 사람의 이름과 생일(월과 일, 예를 들어 0205)를 출력하시오.
 select name, substr(reg_num,3,4) birth from muser;
 
 // 7. 남자의 평균 급여를 구하시오.
-select avg(salary) from muser where substr(reg_num,length(reg_num),1)=1 or substr(reg_num,length(reg_num),1)=3;
+select avg(salary) from muser where substr(reg_num,length(reg_num),1) in (1,3);
 
 // 8. 전체 평균급여보다 높은 급여를 받는 사람의 이름과, 급여를 출력하시오.
 select name, salary from muser where salary>(select avg(salary) from muser);
+-- 평균 급여보다 높은 급여를 받는 사람의 튜플 선택
+-- 컬럼 자체로 해결? 수식? 함수? 서브쿼리?
+-- 평균급여의 결과값으로 조건을 완성 → 서브쿼리
 
 // 9. 전체 평균급여보다 높은 급여를 받는 사람의 이름과, 급여, 평균급여를 출력하시오.
-select name, salary, (select avg(salary) from muser) avgSalary
-from muser
-where salary>(select avg(salary) from muser);
+select name,
+       salary,
+       (select avg(salary) from muser) avgSalary -- 스칼라 서브 쿼리
+    from muser
+    where salary>(select avg(salary) from muser);
+-- 성능은 좋지 못하다.(본쿼리 select에서 튜플을 하나씩 완성해 갈때마다 서브쿼리를 실행)
+-- 동일 서브쿼리를 계속 시행하기 때문이다.
 
 // 10. 여직원의 평균급여보다 높은 남자직원은 모두 몇명입니까
 select sum(count(*))
@@ -91,6 +155,29 @@ select name 이름
     , case when substr(reg_num, length(reg_num), 1)=1 or substr(reg_num, length(reg_num), 1)=3 then '남' 
            when substr(reg_num, length(reg_num), 1)=2 or substr(reg_num, length(reg_num), 1)=4 then '여' end 성별
 from muser;
+
+select name 이름,
+       case when substr(reg_num, length(reg_num), 1) in (1,3) then '남' 
+           else '여'
+       end 성별
+       from muser;
+
+select name 이름,
+       decode(substr(reg_num,8,1),'1','남',3,'남','여') 성별
+       from muser;
+
+select distinct grade, salary from muser;
+-- distinct는 중복된 컬럼을 제거하고 select 절에서 한번만 사용이 가능
+-- 중복제거 범위는 select에서 지정한 전체 행의 중복이다.
+-- #3번 문제에서 연령별(time컬럼) 급여의 합. over 함수 이용
+select time 연령,
+       sum(salary) over(partition by time) 총합
+       from muser;
+       
+select distinct -- distinct는 select절에 사용된 모든 column에 적용되어 중복을 제거한다.
+       time 연령,
+       sum(salary) over(partition by time) 총합
+       from muser;
 
 // 15. time은 근무시간, 근무시간이 31이상인 사람의 이름을 출력하시오.
 select name from muser where time>=31;
@@ -131,6 +218,11 @@ select name
 select sum(12*(select sum(time) from muser where time>=30 and time <=31)) "31세 이하"
     , sum(12*(select sum(time) from muser where time>=32)) "32세 이상"
     from muser;
+    
+select
+    (select trunc(sum(months_between(sysdate,substr(reg_num,1,6)))) from muser where time in (30,31)) as 삼공삼일,
+    (select trunc(sum(months_between(sysdate,substr(reg_num,1,6)))) from muser where time >= 32) as 삼공삼일
+    from dual;
 
 // #3) 연령별 급여의 합, over()함수 이용
 select time, sum(salary) over(partition by time order by time) "급여의 합" from muser;
